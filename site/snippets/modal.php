00b85e433a549093b7b2c88ec650325b4306481b
Jack MB <jack@designandcode.com>
2023-12-06 14:39:31 +0100

    only show window header buttons if opening in modal

--- site/snippets/modal.php
+++ site/snippets/modal.php
@@ -3,7 +3,7 @@
     <div aria-hidden="true" class="absolute top-0 left-0 w-full h-screen bg-default/40 transition duration-300" x-cloak x-show="open" x-transition.opacity x-transition:leave="delay-150"></div>
     <div data-modal-document @click.stop="" x-cloak x-show="open" x-transition:enter="transition ease-out duration-300" x-transition:enter-start="transform translate-y-[120%]" x-transition:enter-end="transform translate-y-0" x-transition:leave="transition ease-out duration-300" x-transition:leave-start="transform translate-y-0" x-transition:leave-end="transform translate-y-[120%]" class="flex flex-col shadow-lg overflow-auto bg-white w-[1030px] h-auto z-40 max-h-[85vh] relative">
       <div class="h-screen">
-        <?php snippet('window', ['title' => $title, 'subheading' => $subheading], slots: true) ?>
+        <?php snippet('window', ['title' => $title, 'subheading' => $subheading, 'modal' => true], slots: true) ?>
         <!-- decide what to show depending on content type -->
 
         <div class="p-4 max-h-[75vh]">
