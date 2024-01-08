78aee1df40cc9dcaf37b43d245cf89e73b9b8393
Jack MB <jack@designandcode.com>
2024-01-08 15:06:28 +0100

    add filters to homepage controller

--- site/controllers/home.php
+++ site/controllers/home.php
@@ -4,5 +4,15 @@ return function ($page, $pages, $site, $kirby) {
   $shared = $kirby->controller('site', compact('page', 'pages', 'site', 'kirby'));
   $titleTag = $site->title();
   $metaDescription = $site->seoDescription();
-  return A::merge($shared, compact('titleTag', 'metaDescription'));
+
+  $categoryPages = $site->page("project-categories")->children()->published();
+  $categories = [];
+  foreach ($categoryPages as $category) {
+    $title = $category->title();
+    array_push($categories, $title);
+  }
+  $status = ['Active', 'Paused'];
+  $seekingParticipants = ['Yes', 'No'];
+
+  return A::merge($shared, compact('titleTag', 'metaDescription', 'categories', 'status', 'seekingParticipants'));
 };
