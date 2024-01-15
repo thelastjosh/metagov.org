61894d893e421f28261bbe6baf284d4a23beaa38
Jack MB <jack@designandcode.com>
2024-01-15 12:38:42 +0100

    set date in footer to be dynamic

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -3,7 +3,7 @@
 <footer class="grid grid-cols-3 w-full px-6 py-[30px]">
   <span class="text-brand mb-0">Metagov –– </span>
   <?php snippet('color-mode') ?>
-  <span class="text-brand text-right mb-0">–– 2023</span>
+  <span class="text-brand text-right mb-0">–– <?= date("Y"); ?></span>
 </footer>
 
 </body>
