a04222e89cf1491988fcfe9ad43f50fed057faa1
Jack MB <jack@designandcode.com>
2024-02-13 17:42:11 +0100

    fix bug with ajax modal not closing on cross click

--- site/snippets/window.php
+++ site/snippets/window.php
@@ -20,7 +20,7 @@
       <div class="border-t border-secondary dark:border-secondary-dark "></div>
     </div>
     <?php if (!empty($modal)) : ?>
-      <button @click="open = false">
+      <button <?php if ($ajax) : ?> @click="$dispatch('toggle_modal')" <?php else : ?> @click="open = false" <?php endif ?>>
         <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path fill-rule="evenodd" clip-rule="evenodd" d="M6.846 6L8.82 4.026C8.934 3.918 9 3.768 9 3.6C9 3.27 8.73 3 8.4 3C8.232 3 8.082 3.066 7.974 3.174L6 5.154L4.026 3.174C3.918 3.066 3.768 3 3.6 3C3.27 3 3 3.27 3 3.6C3 3.768 3.066 3.918 3.174 4.026L5.154 6L3.18 7.974C3.066 8.082 3 8.232 3 8.4C3 8.73 3.27 9 3.6 9C3.768 9 3.918 8.934 4.026 8.826L6 6.846L7.974 8.82C8.082 8.934 8.232 9 8.4 9C8.73 9 9 8.73 9 8.4C9 8.232 8.934 8.082 8.826 7.974L6.846 6Z" fill="#008060" />
           <rect x="0.5" y="0.5" width="11" height="11" stroke="#008060" />
