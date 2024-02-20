aae57f2107d6a59e33932588eb2f6702ea81b064
Jack Murray-Brown <hi@jackmurraybrown.com>
2024-02-20 14:28:30 +0100

    make project status filter tags dynamic (#20)

--- site/controllers/projects.php
+++ site/controllers/projects.php
@@ -4,7 +4,7 @@ return function ($page, $pages, $site, $kirby) {
   $shared = $kirby->controller('site', compact('page', 'pages', 'site', 'kirby'));
   $categories = $page->children()->pluck("category", ",", true);
   $types = $page->children()->pluck("type", ",", true);
-  $status = ['Active', 'Paused'];
+  $status = $page->children()->pluck("project_status", ",", true);;
   $seekingParticipants = ['Yes', 'No'];
   return A::merge($shared, compact('categories', 'types', 'status', 'seekingParticipants'));
 };
