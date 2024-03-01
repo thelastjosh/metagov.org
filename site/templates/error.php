d4fbaf8b67cf4b74e262d11cf62b49d13a7b2973
Jack MB <jack@designandcode.com>
2024-03-01 16:12:14 +0100

    add basic error page

---
+++ site/templates/error.php
@@ -0,0 +1,12 @@
+<?php snippet('header') ?>
+<div class="container max-w-3xl py-8">
+  <div class="mb-8">
+    <h1 class="text-xxl mb-2">Error</h1>
+    <h2 class="text-large">
+      Page not found
+    </h2>
+  </div>
+
+  <a class="underline" href="/">Return home</a>
+</div>
+<?php snippet('footer') ?>
