50c5b9e4149c2e7d5b9e02a956ffce717dea3acd
Jenny Fan <fan.jenny@gmail.com>
2025-03-25 16:40:00 -0400

    Reconfigure Admin, Editor, and Member user roles

--- site/config/config.php
+++ site/config/config.php
@@ -70,7 +70,7 @@ return [
   'sitemap.ignore' => ['error'],
   // tell the plugin which `role => fieldname` associations to use
   'sylvainjule.bouncer.list' => [
-    'director' => [ // matches your blueprint filename without .yml
+    'editor' => [ // matches your blueprint filename without .yml
         'fieldname' => 'canaccess',
         'nav' => true
     ]
